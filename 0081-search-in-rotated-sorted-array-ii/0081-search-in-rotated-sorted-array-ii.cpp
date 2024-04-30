c39bfdc4e32b5b88b6bcc56d44a31b39c69fac72
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-04-30 17:55:49 +0530

    Time: 7 ms (32.54%), Space: 16.6 MB (28.82%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=1;
+        int high=n-2;
+        int mid;
+        if(nums[0]==target || nums[n-1]==target) return true;
+        while(low<=high)
+        {
+           mid=(low+high)/2;
+           if(nums[mid]==target) return true;
+            
+           if(nums[low]==nums[mid]) // new condition to handle duplicates
+           {
+               low++;
+               continue;
+           }
+           
+           // here we are first checking for sorted array if the array is sorted then only we can apply binary search
+           // after getting the sorted part we check if the target exists between them if yes then apply binary search if not then exclude the sorted array 
+           if(nums[low]<=nums[mid]) // checking if the left half is sorted
+           {
+            if(nums[low]<=target && nums[mid]>=target) high=mid-1; // checking if the target lies in the range of sorted array
+             // excluding mid as we have already checked if target is equal to nums[mid]
+            else low=mid+1;
+           }
+           
+           else   // for sure the right part will be sorted
+           {
+                if(nums[mid]<=target && target<=nums[high]) low=mid+1;
+                else high=mid-1;
+           }
+
+        }
+        return false;
+    }
+};
