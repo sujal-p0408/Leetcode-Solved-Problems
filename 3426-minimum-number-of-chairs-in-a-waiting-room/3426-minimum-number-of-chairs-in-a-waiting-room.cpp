5c96e37db7e4c12d98406bf821b7f01e1e9c6eb5
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-06-03 20:46:16 +0530

    Time: 2 ms (60%), Space: 7.9 MB (40%) - LeetHub

---
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/3426-minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumChairs(string s) {
+        int n=s.length();
+        int ans=0;
+          int cnt=0;
+        for(int i=0;i<n;i++)
+        {
+          if(s[i]=='E')
+          {
+            cnt++;
+          }
+          else 
+          {
+            cnt--;
+          }
+          ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
