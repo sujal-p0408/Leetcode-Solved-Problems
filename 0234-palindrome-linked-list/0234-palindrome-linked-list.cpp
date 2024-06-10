d0443c403e701820466bc5279be09d2c4823cd4e
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-06-10 13:12:22 +0530

    Time: 144 ms (92.58%), Space: 119 MB (59.07%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,19 +10,57 @@
  */
 class Solution {
 public:
+ListNode* reverseLinkedList(ListNode* head) {
+    // Base case:
+    // If the linked list is empty or has only one node,
+    // return the head as it is already reversed.
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+    
+    // Recursive step:
+    // Reverse the linked list starting 
+    // from the second node (head->next).
+    ListNode* newHead = reverseLinkedList(head->next);
+    
+    // Save a reference to the node following
+    // the current 'head' node.
+    ListNode* front = head->next;
+    
+    // Make the 'front' node point to the current
+    // 'head' node in the reversed order.
+    front->next = head;
+    
+    // Break the link from the current 'head' node
+    // to the 'front' node to avoid cycles.
+    head->next = NULL;
+    
+    // Return the 'newHead,' which is the new
+    // head of the reversed linked list.
+    return newHead;
+}
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp=head;
+       ListNode* slow=head;
+       ListNode* fast=head;
 
-        while(temp!=NULL)
-        {
-            arr.push_back(temp->val);
-            temp=temp->next;
-        }
-      
-      vector<int> arr_old=arr;
-      reverse(arr.begin(),arr.end());
-      if(arr_old==arr) return true;
-      return false;
+       while(fast->next!=NULL && fast->next->next!=NULL)
+       {
+        slow=slow->next;
+        fast=fast->next->next;
+       }
+
+       ListNode* newHead = reverseLinkedList(slow->next);
+
+       ListNode* first=head;
+       ListNode* second=newHead;
+
+       while(second!=NULL)
+       {
+        if(first->val != second->val) return 0;
+        first=first->next;
+        second=second->next;
+       }
+
+       return true;
     }
 };
