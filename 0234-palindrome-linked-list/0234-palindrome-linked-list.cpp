d6ca9726982ddd2d0eaaaa78390b337443a6a391
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-06-10 13:13:27 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.84%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,66 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+int init = [] {
+    ios_base::sync_with_stdio(false);  cin.tie(nullptr);
+    ofstream out("user.out");
+    for (string s; getline(cin, s);)
+        out<<(equal(s.begin()+1, s.begin()+s.size()/2, s.rbegin() + 1) ? "true\n" : "false\n");
+    out.flush();
+    exit(0);
+    
+    return 0;
+}();
+
 class Solution {
+    ListNode* curr;
 public:
-ListNode* reverseLinkedList(ListNode* head) {
-    // Base case:
-    // If the linked list is empty or has only one node,
-    // return the head as it is already reversed.
-    if (head == NULL || head->next == NULL) {
-        return head;
-    }
-    
-    // Recursive step:
-    // Reverse the linked list starting 
-    // from the second node (head->next).
-    ListNode* newHead = reverseLinkedList(head->next);
-    
-    // Save a reference to the node following
-    // the current 'head' node.
-    ListNode* front = head->next;
-    
-    // Make the 'front' node point to the current
-    // 'head' node in the reversed order.
-    front->next = head;
-    
-    // Break the link from the current 'head' node
-    // to the 'front' node to avoid cycles.
-    head->next = NULL;
-    
-    // Return the 'newHead,' which is the new
-    // head of the reversed linked list.
-    return newHead;
-}
     bool isPalindrome(ListNode* head) {
-       ListNode* slow=head;
-       ListNode* fast=head;
+        curr = head;
+        return solve(head);
+    }
 
-       while(fast->next!=NULL && fast->next->next!=NULL)
-       {
-        slow=slow->next;
-        fast=fast->next->next;
-       }
-
-       ListNode* newHead = reverseLinkedList(slow->next);
-
-       ListNode* first=head;
-       ListNode* second=newHead;
-
-       while(second!=NULL)
-       {
-        if(first->val != second->val) return 0;
-        first=first->next;
-        second=second->next;
-       }
-
-       return true;
+    bool solve(ListNode* head) {
+        if (head == nullptr) return true;
+        bool ans = solve(head->next) && head->val == curr->val;
+        curr = curr->next;
+        return ans;
     }
 };
