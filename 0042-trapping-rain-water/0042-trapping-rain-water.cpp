1f1b58a8c62cd131b3f7869958cdfe0a9dd318f7
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-06-17 19:37:43 +0530

    Time: 44 ms (5.04%), Space: 23.6 MB (6.76%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        vector<int> maxHeight(n);
+
+        int left_max=height[0];
+        int right_max=height[n-1];
+
+        for(int i=0;i<n;i++)
+        {
+        //   maxHeight[i]=min(left_max[i])
+             left_max=max(left_max,height[i]);
+             left[i]=left_max;
+             cout<<left[i]<<" ";
+        }
+        cout<<endl;
+          for(int i=n-1;i>=0;i--)
+        {
+        //   maxHeight[i]=min(left_max[i])
+             right_max=max(right_max,height[i]);
+             right[i]=right_max;
+             cout<<left[i]<<" ";
+        }
+ cout<<endl;
+        for(int i=0;i<n;i++)
+        {
+            maxHeight[i]=min(left[i],right[i])-height[i];
+        }
+    int ans=accumulate(maxHeight.begin(),maxHeight.end(),0);
+        return ans;
+    }
+};
