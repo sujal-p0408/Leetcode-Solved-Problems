719747ab294a353739bcc8f227f283a15d82aef1
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-06-22 15:12:01 +0530

    Time: 13 ms (67.57%), Space: 14.9 MB (9.95%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,20 +1,82 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        long long ans=0;
-        int mini;
-        int maxi;
-        for(int i=0;i<nums.size();i++)
+        return sumSubarrayMax(nums)- sumSubarrayMins(nums);
+    }
+    long long sumSubarrayMins(vector<int>& arr) {
+        int n=arr.size();
+        vector<long long> left(n,0), right(n,0);
+        stack<pair<long long,long long>> leftSt;
+        stack<pair<long long,long long>> rightSt;
+
+        for(int i=0;i<n;i++)
         {
-            mini=nums[i];
-            maxi=nums[i];
-            for(int j=i;j<nums.size();j++)
+            int cnt=1;
+            while(!leftSt.empty() && leftSt.top().first>arr[i])
             {
-                mini=min(mini,nums[j]);
-                maxi=max(maxi,nums[j]);
-                ans+=(maxi-mini);
+                cnt+=leftSt.top().second;
+                leftSt.pop();
             }
+            
+            left[i]=cnt;
+            leftSt.push({arr[i],cnt});
         }
-        return ans;
+
+        for(int i=n-1;i>=0;i--)
+        {
+            int cnt=1;
+            while(!rightSt.empty() && rightSt.top().first>=arr[i])
+            {
+                cnt+=rightSt.top().second;
+                rightSt.pop();
+            }
+            
+            right[i]=cnt;
+            rightSt.push({arr[i],cnt});
+        }
+    long long ans=0;
+    for(int i=0;i<n;i++)
+    {
+        ans=(ans+( arr[i]*(1LL)*(left[i]*right[i])));
+    }
+    return ans;
+    }
+
+    long long sumSubarrayMax(vector<int>& arr) {
+        int n=arr.size();
+       vector<long long> left(n,0), right(n,0);
+        stack<pair<long long,long long>> leftSt;
+        stack<pair<long long,long long>> rightSt;
+        for(int i=0;i<n;i++)
+        {
+            int cnt=1;
+            while(!leftSt.empty() && leftSt.top().first<arr[i])
+            {
+                cnt+=leftSt.top().second;
+                leftSt.pop();
+            }
+            
+            left[i]=cnt;
+            leftSt.push({arr[i],cnt});
+        }
+
+        for(int i=n-1;i>=0;i--)
+        {
+            int cnt=1;
+            while(!rightSt.empty() && rightSt.top().first<=arr[i])
+            {
+                cnt+=rightSt.top().second;
+                rightSt.pop();
+            }
+            
+            right[i]=cnt;
+            rightSt.push({arr[i],cnt});
+        }
+     long long ans=0;
+    for(int i=0;i<n;i++)
+    {
+        ans=(ans+( arr[i]*(1LL)*(left[i]*right[i])));
+    }
+    return ans;
     }
 };
