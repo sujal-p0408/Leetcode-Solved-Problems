d57d3f533df8c1cff1c4a60a8e4cce86d67625da
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-02 13:21:59 +0530

    Time: 162 ms (82.12%), Space: 137 MB (89.05%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -6,7 +6,7 @@ public:
   for (int i = 0; i < nums.size(); i++) {
     if (!dq.empty() && dq.front() == i - k) dq.pop_front();
 
-    while (!dq.empty() && nums[dq.back()] < nums[i])
+    while (!dq.empty() && nums[dq.back()] <= nums[i])
       dq.pop_back();
 
     dq.push_back(i);
