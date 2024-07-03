afe376ee1794cc2d717e2da66e0d53d2d384717a
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-03 17:53:12 +0530

    Time: 7 ms (48.76%), Space: 16 MB (49.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,22 +1,63 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int> ans={-1,-1};
+int findFirst(vector<int>& nums, int target)
+{
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    int ans=-1;
 
-        if (n == 0) {
-            return ans; // Handle the case when the input vector is empty.
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+
+        if(nums[mid]==target)
+        {
+            ans=mid;
+            high=mid-1;
         }
 
-        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int ub=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-     
-     
-     if(lb==n || nums[lb]!=target)
-    {
-        return ans;
+        else if(nums[mid]<target)
+        {
+            low=mid+1;
+        }
+
+        else high=mid-1;
     }
 
-      return {lb,ub-1};
+    return ans;
+}
+int findSecond(vector<int>& nums, int target)
+{
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    int ans=-1;
+
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+
+        if(nums[mid]==target)
+        {
+            ans=mid;
+            low=mid+1;
+        }
+
+        else if(nums[mid]<target)
+        {
+            low=mid+1;
+        }
+
+        else high=mid-1;
+    }
+
+    return ans;
+}
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int x=findFirst(nums,target);
+        int y=findSecond(nums,target);
+
+        return {x,y};
     }
 };
