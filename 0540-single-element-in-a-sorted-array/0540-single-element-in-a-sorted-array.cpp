79f725a5ade78ccf633e370be2a6bee1d66622fb
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-04 16:38:13 +0530

    Time: 21 ms (41.07%), Space: 24.7 MB (13.95%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -16,14 +16,7 @@ public:
 
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
             return nums[mid];
-            
-            //  if(mid%2!=0)
-            //  {
-            //     if(nums[mid]==nums[mid+1]) r=mid-1;
-            //     else 
-            //  }
-  
-                 if(mid%2!=0 && nums[mid]==nums[mid+1]) r=mid-1;
+            if(mid%2!=0 && nums[mid]==nums[mid+1]) r=mid-1;
             else if(mid%2==0 && nums[mid]==nums[mid-1]) r=mid-2;
             else if(mid%2==0 && nums[mid]==nums[mid+1]) l=mid+2;
             else if(mid%2!=0 && nums[mid]==nums[mid-1]) l=mid+1;
