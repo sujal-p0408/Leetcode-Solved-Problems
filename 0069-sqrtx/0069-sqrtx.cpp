00e0d038fde4cb8a62746152e577949d2241f18d
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-05 19:26:05 +0530

    Time: 5 ms (34.46%), Space: 7.4 MB (80.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,20 +3,20 @@ public:
     int mySqrt(int x) {
         int l=1;
         int r=x;
-        int ans=r;
+        // int ans=r;
         while(l<=r)
         {
             long long mid=l+(r-l)/2;
             long long  val=mid*mid;
             if(val<= (long long) x)
             {
-              ans=mid;
+            //   ans=mid;
               l=mid+1;
             }
 
             else r=mid-1;
         }
 
-        return ans;
+        return r;
     }
 };
