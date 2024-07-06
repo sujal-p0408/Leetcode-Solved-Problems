44720a279df084cf8b8f08a628c195665342d38e
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-06 15:40:11 +0530

    Time: 49 ms (46.85%), Space: 17.4 MB (50.51%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int x) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        int i=0;
+        int j=m-1;
+
+        while(i>=0 && i<n && j>=0 && j<m)
+        {
+            if(matrix[i][j]==x) return true;
+
+            else if(matrix[i][j]>x) j--;
+
+            else i++;
+        }
+
+        return false;
+    }
+};
