598e03e36e314902c774eb4c2b5b4412f8dd21d6
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-28 20:28:39 +0530

    Time: 0 ms (100%), Space: 10.5 MB (18.83%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
         bool check(vector<int>& A) {
-        for (int i = 0, k = 0; i < A.size(); ++i)
-            if (A[i] > A[(i + 1) % A.size()] && ++k > 1)
-                return false;
-        return true;
+            int cnt=0;
+        for (int i = 0; i < A.size(); ++i)
+           { if (A[i] > A[(i + 1) % A.size()])
+                cnt++;
+           }
+        return cnt<=1;
     }
 };
