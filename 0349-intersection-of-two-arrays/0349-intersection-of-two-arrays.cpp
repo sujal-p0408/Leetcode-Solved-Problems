b08c414d899f38ba6ab218fc04e4b8956c6c28a3
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-07-31 19:16:23 +0530

    Time: 4 ms (65.92%), Space: 13.4 MB (36.36%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& arr1, vector<int>& arr2) {
-    int i=0;
-	int j=0;
-    int n=arr1.size();
-    int m=arr2.size();
-    vector<int> ans;
-    unordered_set<int> u_set;
-    sort(arr1.begin(),arr1.end());
-    sort(arr2.begin(),arr2.end());
-	while(i<n && j<m)
-	{
-      if(arr1[i]==arr2[j]) 
-	  {
-		  u_set.insert(arr1[i]);
-		  i++;
-		  j++;
-	  }
-
-	  else if(arr1[i]<arr2[j]) i++;
-	  else j++;
-	}
-
-    for(auto i: u_set)
-    {
-        ans.push_back(i);
-    }
-	return ans;
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> m(nums1.begin(), nums1.end());
+        vector<int> res;
+        for (auto a : nums2)
+            if (m.count(a)) {
+                res.push_back(a);
+                m.erase(a);
+            }
+        return res;
     }
 };
