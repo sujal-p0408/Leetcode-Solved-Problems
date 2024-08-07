6f6d802f37d584acc689910e86f6025268a8afc8
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-08-07 21:51:26 +0530

    Time: 3 ms (61.97%), Space: 14.9 MB (24.18%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int idx=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                idx=i;
+                break;
+            }
+        }
+         if(idx==-1)
+         {
+            reverse(nums.begin(),nums.end());
+            
+         }
+        else {for(int i=n-1;i>=idx;i--)
+        {
+            if(nums[i]>nums[idx])
+            {
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+
+        sort(nums.begin()+idx+1,nums.end());
+}
+        for(auto &i: nums)
+        {
+            cout<<i<<" ";
+        }
+
+        cout<<endl;
+    }
+};
