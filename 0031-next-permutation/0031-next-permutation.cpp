ed76c5dd181cfd7cf700e7e1c0fef8bbb042c04c
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-08-11 13:17:42 +0530

    Time: 0 ms (100%), Space: 14.9 MB (28%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,32 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        return;
+    void nextPermutation(vector<int>& arr) {
+        int n=arr.size();
+        int idx=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(arr[i]<arr[i+1]) 
+            {
+                idx=i;
+                break;
+            }
+        }
+
+if(idx==-1)
+{
+    sort(arr.begin(),arr.end());
+//    reverse(arr.begin(),arr.end());
+    return;
+}
+        for(int i=n-1;i>=0;i--)
+        {
+            if(arr[i]>arr[idx])
+            {
+                swap(arr[i],arr[idx]);
+                break;
+            }
+        }
+
+        sort(arr.begin()+idx+1,arr.end());
     }
 };
