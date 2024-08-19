4b9a74c4178f1264f7bd1c5c85942c1cc2c3c053
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-08-19 15:27:14 +0530

    Time: 4 ms (95.56%), Space: 16.9 MB (26.64%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,9 +4,10 @@ public:
         int n=matrix.size();
         int m=matrix[0].size();
 
-        vector<int> row(n,0);
-        vector<int> col(m,0);
-
+        // vector<int> row(n,0); => matrix[...][0]
+        // vector<int> col(m,0); => matrix[0][...]
+        
+        int col0=0;
 
         for(int i=0;i<n;i++)
         {
@@ -14,21 +15,25 @@ public:
             {
                 if(matrix[i][j]==0)
                 {
-                  row[i]=-1;
-                  col[j]=-1;
+                  matrix[i][0]=0;  //for rows we are directly marking them
+
+                  if(j!=0) matrix[0][j]=0; //but for col it's collding hence we will directly mark only the others col (except [0][0])
+                  
+                  else col0=1;
                 }
             }
         }
 
-         for(int i=0;i<n;i++)
+         for(int i=1;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=1;j<m;j++)
             {
-                if(row[i]==-1 || col[j]==-1)
-                {
-                    matrix[i][j]=0;
-                }
+               if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
             }
         }
+
+        if(matrix[0][0]==0) for(int j=1;j<m;j++) matrix[0][j]=0;
+
+        if(col0==1) for(int i=0;i<n;i++) matrix[i][0]=0;
     }
 };
