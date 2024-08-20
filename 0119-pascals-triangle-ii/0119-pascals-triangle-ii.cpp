e7703842bc643ccd9ba68c45976fec9656a7bb42
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-08-20 17:57:34 +0530

    Time: 0 ms (100%), Space: 7.8 MB (39.03%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        vector<int> ans;
+        long long res=1;
+        ans.push_back(res);
+
+        for(int i=1;i<=n;i++)
+        {
+            res*=(n-i+1);
+            res/=i;
+            ans.push_back(res);
+        }
+
+        return ans;
+    }
+};
