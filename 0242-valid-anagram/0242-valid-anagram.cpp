9c8edc37ec5c7a6ec919bafc5b505f6eadf48f2b
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-09-19 22:02:25 +0530

    Time: 2 ms (98.56%), Space: 8.8 MB (55.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,13 +3,20 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()) return false;
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+       int freq[26]={0};
 
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]!=t[i]) return false;
-        }
+       for(int i=0;i<s.size();i++)
+       {
+        freq[s[i] - 'a']++;
+
+
+        freq[t[i] - 'a']--;
+       }
+
+       for(int i=0;i<26;i++)
+       {
+        if(freq[i]!=0) return false;
+       }
 
         return true;
     }
