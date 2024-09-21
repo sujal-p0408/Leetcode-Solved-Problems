37e399fc67235046fe9bb460811fdb05424b3fed
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-09-21 14:47:40 +0530

    Time: 13 ms (38.75%), Space: 11.1 MB (54.93%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -2,23 +2,25 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char,int> umap;
-        // int n=s.length();
-
         for(auto i: s) umap[i]++;
-        
+
         vector<pair<char,int>> v;
 
-        for(auto& [c,freq] : umap)  v.push_back(make_pair(c, freq));
-        sort(v.begin(),v.end(), [&](auto a, auto b) 
+        for(auto [c,freq]: umap)
+        v.push_back(make_pair(c,freq));
+
+        sort(v.begin(),v.end(), [](auto a, auto b)
         {
             return a.second>b.second;
-        });
-
+        }
+        );
 
         string ans;
-        for (auto& [c, freq] : v)
-            ans.append(freq, c);
-        return ans;
-
+        for(auto [c,freq]: v)
+        {
+            ans.append(freq,c);
+        } 
+       
+       return ans; 
     }
 };
