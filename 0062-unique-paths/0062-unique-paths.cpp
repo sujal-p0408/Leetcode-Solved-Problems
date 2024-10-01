fec76baca756d8d51c413bad76c33090b95224e7
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-10-01 13:28:15 +0530

    Time: 2 ms (41.72%), Space: 7.5 MB (90.05%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if(m == 1 || n == 1)
-            return 1;
-        m--;
-        n--;
-        if(m < n) {              // Swap, so that m is the bigger number
-            m = m + n;
-            n = m - n;
-            m = m - n;
+        if(m==1 || n==1) return 1;
+        
+        m--,n--;
+
+        if(m<n)
+        {
+            m=m+n;
+            n=m-n;
+            m=m-n;
         }
-        long res = 1;
-        int j = 1;
-        for(int i = m+1; i <= m+n; i++, j++){       // Instead of taking factorial, keep on multiply & divide
-            res *= i;
-            res /= j;
+
+        long long ans=1;
+        int j=1;
+        for(int i=m+1;i<=m+n;i++,j++)
+        {
+            ans*=i;
+            ans/=j;
         }
-            
-        return (int)res;
+
+        return ans;
     }
 };
