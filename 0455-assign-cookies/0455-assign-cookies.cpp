bb4b88956ee36b3e81a7d20c5bb27b24b8b7d7b9
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2024-10-18 21:29:53 +0530

    Time: 9 ms (99.89%), Space: 44.8 MB (23.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cookiesNums = s.size();
-        if(cookiesNums == 0)  return 0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
+        int n=g.size();
+        int m=s.size();
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
 
-        int maxNum = 0;
-        int cookieIndex = cookiesNums - 1;
-        int childIndex = g.size() - 1;
-        while(cookieIndex >= 0 && childIndex >=0){
-            if(s[cookieIndex] >= g[childIndex]){
-                maxNum++;
-                cookieIndex--;
-                childIndex--;
+        int l=0,r=0;
+        int ans=0;
+        while(r<n && l<m)
+        {
+            if(g[r]<=s[l])
+            {
+                ans++;
+                r++;
             }
-            else{
-                childIndex--;
-            }
+            l++;
         }
-        return maxNum;
+
+        return ans;
     }
 };
