8b8a08be57595ddb8a6659b7240a284d172da816
sujal-p0408 <118656055+sujal-p0408@users.noreply.github.com>
2025-06-24 14:48:04 +0530

    Time: 0 ms (100%), Space: 18.1 MB (61.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int i, int j, int oldclr, int color, vector<vector<int>>& vis)
+    void dfs(vector<vector<int>>& image, int i, int j, int oldclr, int color)
     {
           int n=image.size();
         int m=image[0].size();
 
-        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=oldclr) return;
+        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=oldclr || image[i][j]==color) return;
          
-         vis[i][j]=1;
+        //  vis[i][j]=1;
         image[i][j]=color;
 
-        dfs(image, i-1, j, oldclr, color, vis);
-        dfs(image, i+1, j, oldclr,  color, vis);
-        dfs(image, i, j-1, oldclr,  color, vis);
-        dfs(image, i, j+1, oldclr,  color, vis);
+        dfs(image, i-1, j, oldclr, color);
+        dfs(image, i+1, j, oldclr,  color);
+        dfs(image, i, j-1, oldclr,  color);
+        dfs(image, i, j+1, oldclr,  color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int i, int j, int color) {
         int n=image.size();
         int m=image[0].size();
 
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        // vector<vector<int>> vis(n, vector<int>(m, 0)); {no need of visited array}
         
         int oldclr=image[i][j];
        
                 if( oldclr!=color)
                 {
-                    vis[i][j]=1;
+                    // vis[i][j]=1;
                     
-                    dfs(image, i, j,oldclr, color, vis);
+                    dfs(image, i, j,oldclr, color);
                 }
      
 
